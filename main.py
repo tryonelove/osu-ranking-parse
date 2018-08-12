fdcea042d44688df04cd62e077322d8c90671523
tryonelove <try13crew@gmail.com>
2018-08-12 21:13:34 +0300

    full rewrite

--- main.py
+++ main.py
@@ -2,57 +2,69 @@ import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 
-number_of_pages = 41
-url = 'https://osu.ppy.sh/p/pp/?m=0&s=3&o=1&f=&page='
 playcounts = []
 pps = []
 accuracy = []
 ss = []
-def get_html(url):
-    r = requests.get(url).text
-    return r
 
-def playcount_parse():
-    for i in range(number_of_pages):
-        soup = BeautifulSoup(get_html(url+str(i)), 'lxml')
-        playcount = soup.findAll('td')
-        for user in range(3,len(playcount),8):
-            pc = str(playcount[user])
-            playcounts.append(pc[10:pc.find('(') - 1].replace(',',''))
+class Parse:
+    def __init__(self, number_of_pages, country):
+        self.number_of_pages = number_of_pages 
+        self.country = country
+        self.url = 'https://osu.ppy.sh/rankings/osu/performance?country={}&page={}'.format(country, self.number_of_pages)
+
+    def get_html(self, url):
+        r = requests.get(self.url).text
+        return r
+
+    def playcount(self):
+        for i in range(1, self.number_of_pages + 1):
+            soup = BeautifulSoup(self.get_html(self.url + str(i)), 'html.parser')
+            playcount = soup.findAll('td')
+            for user in range(3,len(playcount),8):
+                pc = "".join(playcount[user].text.replace(',', '').split())
+                playcounts.append(pc)
+            print("Page "+str(i)+" of "+str(self.number_of_pages)+" for playcount")
+
+    def pp(self):
+        for i in range(1, self.number_of_pages + 1):
+            soup = BeautifulSoup(self.get_html(self.url + str(i)), 'html.parser')
+            pp = soup.findAll('td')
+            for user in range(4, len(pp), 8):
+                pepe = "".join(pp[user].text.replace(',', '').split())
+                pps.append(pepe)
+            print("Page "+str(i)+" of "+str(self.number_of_pages)+" for PP")
+
+    def accuracy(self):
+        for i in range(1, self.number_of_pages + 1):
+            soup = BeautifulSoup(self.get_html(self.url + str(i)), 'html.parser')
+            acc = soup.findAll('td')
+            for user in range(2, len(acc), 8):
+                accur  = "".join(acc[user].text.replace(',', '').split())
+                accuracy.append(accur)
+            print("Page "+str(i)+" of "+str(self.number_of_pages)+" for Accuracy")
+
+    def ss(self):
+        for i in range(1, self.number_of_pages + 1):
+            soup = BeautifulSoup(self.get_html(self.url + str(i)), 'html.parser')
+            ss_count = soup.findAll('td')
+            for user in range(5, len(ss_count), 8):
+                ss1 = "".join(ss_count[user].text.replace(',', '').split())
+                ss.append(ss1)
+            print("Page "+str(i)+" of "+str(self.number_of_pages)+" for SS")
 
 
-def pp_parse():
-    for i in range(number_of_pages):
-        soup = BeautifulSoup(get_html(url + str(i)), 'lxml')
-        pp = soup.findAll('td')
-        for user in range(4, len(pp), 8):
-            pepe = str(pp[user])
-            pps.append(pepe[36:pepe.find('pp')].replace(',',''))
+def main():
+    parse = Parse(5, 'RU')
+    parse.pp()
+    parse.playcount()
+    x = [int(a) for a in playcounts]
+    y = [int(b) for b in pps]
+    plt.scatter(x, y, s=20)
+    plt.title("f(x) = pp(pc)")
+    plt.xlabel("PP")
+    plt.ylabel('PC')
+    plt.show()
 
-def accuracy_parse():
-    for i in range(number_of_pages):
-        soup = BeautifulSoup(get_html(url + str(i)), 'lxml')
-        acc = soup.findAll('td')
-        for user in range(2, len(acc), 8):
-            accur = str(acc[user])
-            accuracy.append(accur[4:accur.find('%')])
-
-def ss_parse():
-    for i in range(number_of_pages):
-        soup = BeautifulSoup(get_html(url + str(i)), 'lxml')
-        ss_count = soup.findAll('td')
-        for user in range(5, len(ss_count), 8):
-            ss1 = str(ss_count[user])
-            ss.append(ss1[19:ss1.find('</')])
-
-ss_parse()
-# playcount_parse()
-pp_parse()
-# playcount_parse()
-x = [float(a) for a in pps]
-y = [int(b) for b in ss]
-plt.scatter(x, y, s=20)
-plt.title("f(x)")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.show()
+if __name__ == '__main__':
+    main()
